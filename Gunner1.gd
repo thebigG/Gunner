4093ec2fe36ddfa7cd589cd3b94bc1760886992f
thebigg <yunior.eury@gmail.com>
2021-06-13 18:08:26 -0500

    -Got left and right animations :)

--- Gunner1.gd
+++ Gunner1.gd
@@ -12,13 +12,32 @@ func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	$Turn.set_frame(0)
 	if Input.is_action_pressed("ui_right"):
+			$Turn.set_animation("Right")
+			$Turn.play()
+			$Turn.set_frame(1)
+			print('current animation:' + $Turn.animation)
 			current_direction.x = speed
 			current_direction.y = 0
+	
+	#Would like a cleaner way of doing this...
+	else:
+		if $Turn.animation == "Right":
+			$Turn.stop()
+			$Turn.set_frame(0)
 
 	if Input.is_action_pressed("ui_left"):
+			$Turn.set_animation("Left")
+			$Turn.play()
+			$Turn.set_frame(1)
 			current_direction.x = -speed
 			current_direction.y = 0
+	else:
+		if $Turn.animation == "Left":
+			$Turn.stop()
+			$Turn.set_frame(0)
+		
 	if Input.is_action_pressed("ui_down"):
 			current_direction.y = speed
 			current_direction.x = 0
