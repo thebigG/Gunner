aeffaadf189c715bf902d9a453bf4eceecc86a76
thebigg <yunior.eury@gmail.com>
2021-06-23 21:44:39 -0500

    -Added bullet(still needs to be scripted)

--- Gunner1.gd
+++ Gunner1.gd
@@ -40,6 +40,9 @@ func _physics_process(delta):
 		if $Turn.animation == "Left":
 			$Turn.stop()
 			$Turn.set_frame(0)
+	
+	if Input.is_action_pressed("ui_accept"):
+		print("shoot")
 
 
 #	Figure out a way to limit the viewport for the player
@@ -47,10 +50,10 @@ func _physics_process(delta):
 	
 #	TODO: Figure out a cleaner way of clamping
 	position.x = clamp(position.x, 100, screen_size.size.x-24)
-	print("screen_size-->" + str(screen_size.size.x))
-	
-	print("posittion-->"+ str(position))
-	print("clamped pos-->"+ str(clamp(position.x, 0, screen_size.size.x)))
+#	print("screen_size-->" + str(screen_size.size.x))
+#
+#	print("posittion-->"+ str(position))
+#	print("clamped pos-->"+ str(clamp(position.x, 0, screen_size.size.x)))
 #	position.x = clamp(position.x, 0, 1)
 
 
