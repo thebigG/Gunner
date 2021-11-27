d14f53f73d502bec41ada28d2eccb51650f6e0d9
thebigg <yunior.eury@gmail.com>
2021-11-26 19:38:01 -0600

    -Gunner can move in the Y axis now without losing the parallax background. Fixes #8.

--- Gunner/Gunner1.gd
+++ Gunner/Gunner1.gd
@@ -16,9 +16,16 @@ func _physics_process(delta):
 		add_child(new_bullet)
 		$Shoot.play()
 		new_bullet.shoot()
-		
 	$Turn.set_frame(0)
+	
 	current_direction.x = 0
+	current_direction.y = 0
+	if Input.is_action_pressed("ui_up"):
+#			$Turn.set_animation("Right")
+#			$Turn.play()
+#			$Turn.set_frame(1)
+			current_direction.y = -speed
+			
 	if Input.is_action_pressed("ui_right"):
 			$Turn.set_animation("Right")
 			$Turn.play()
@@ -40,7 +47,6 @@ func _physics_process(delta):
 		if $Turn.animation == "Left":
 			$Turn.stop()
 			$Turn.set_frame(0)
-
 
 #	Figure out a way to limit the viewport for the player
 	position.x = clamp(position.x, 0, screen_size.size.x-24)
