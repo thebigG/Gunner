7edb39a510aa2b7e5c5ab5cd87589d66c8808d4a
thebigg <yunior.eury@gmail.com>
2021-12-02 23:20:49 -0600

    -Fixes #9.

--- Gunner/Gunner1.gd
+++ Gunner/Gunner1.gd
@@ -2,13 +2,15 @@ extends KinematicBody2D
 
 export(PackedScene) var bullet_scene
 
-var current_direction = Vector2()
+var current_velocity = Vector2()
 var screen_size
-export var speed = 0
+var speed = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect()
-	current_direction.y = -speed
+	speed = get_parent().get_node("EasyStageScene/ParallaxDriver").get("speed")
+	current_velocity.y = -speed
+	
 	
 func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
@@ -18,38 +20,42 @@ func _physics_process(delta):
 		new_bullet.shoot()
 	$Turn.set_frame(0)
 	
-	current_direction.x = 0
-	current_direction.y = 0
+#	current_velocity.x = 0
 	if Input.is_action_pressed("ui_up"):
-			current_direction.y = -speed
-			
-	if Input.is_action_pressed("ui_down"):
-			current_direction.y = speed
-			
-	if Input.is_action_pressed("ui_right"):
-			$Turn.set_animation("Right")
-			$Turn.play()
-			$Turn.set_frame(1)
-			current_direction.x = speed
-	
-	#Would like a cleaner way of doing this... 
+			current_velocity.y = (speed + 10) * (-1)
 	else:
-		if $Turn.animation == "Right":
-			$Turn.stop()
-			$Turn.set_frame(0)
+		current_velocity.y = speed * -1
 
-	if Input.is_action_pressed("ui_left"):
-			$Turn.set_animation("Left")
-			$Turn.play()
-			$Turn.set_frame(1)
-			current_direction.x = -speed
-	else:
-		if $Turn.animation == "Left":
-			$Turn.stop()
-			$Turn.set_frame(0)
+# TODO: Have to rethink this logic a bit since now Gunner has to keep up with the
+# the velocity of the camera.
+#
+#	if Input.is_action_pressed("ui_down"):
+#			current_velocity.y = speed + 10
+#
+#	if Input.is_action_pressed("ui_right"):
+#			$Turn.set_animation("Right")
+#			$Turn.play()
+#			$Turn.set_frame(1)
+#			current_velocity.x = speed
+#
+#	#Would like a cleaner way of doing this... 
+#	else:
+#		if $Turn.animation == "Right":
+#			$Turn.stop()
+#			$Turn.set_frame(0)
+#
+#	if Input.is_action_pressed("ui_left"):
+#			$Turn.set_animation("Left")
+#			$Turn.play()
+#			$Turn.set_frame(1)
+#			current_velocity.x = -speed
+#	else:
+#		if $Turn.animation == "Left":
+#			$Turn.stop()
+#			$Turn.set_frame(0)
 
 #	Figure out a way to limit the viewport for the player
-	move_and_collide(current_direction)
+	move_and_collide(current_velocity)
 	position.x = clamp(position.x, 0, screen_size.size.x-24)
 	
 #	TODO:This does not work properly for now. Gunner starts "fading"....
