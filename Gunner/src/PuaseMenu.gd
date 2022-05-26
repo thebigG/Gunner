fb5f37a89b0248e988e7174bef58578f3532a183
thebigg <yunior.eury@gmail.com>
2022-05-26 00:04:01 -0500

    -Pause menu always appears in the middle now.

--- Gunner/src/PuaseMenu.gd
+++ Gunner/src/PuaseMenu.gd
@@ -10,7 +10,14 @@ func _ready():
 
 func _unhandled_input(input: InputEvent):
 	if input.is_action("ui_pause"):
-		self.rect_position.y = get_parent().get_node("Gunner1").position.y
+#		self.rect_position.y = get_parent().get_node("Gunner1").position.y
+		var driver_pos = get_parent().get_node("EasyStageScene/ParallaxDriver").position.y
+		var middle_pos = (
+			get_parent().get_node("EasyStageScene/EasyStage/ParallaxLayer/Background").get_viewport_rect().size.y
+			/ 2
+		)
+
+		self.rect_position.y = driver_pos - middle_pos
 		self.show()
 		get_tree().paused = true
 
