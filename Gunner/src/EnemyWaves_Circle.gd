b5ab254c443d9a17d83c8b91bcbfae29ebd7cf55
thebigG <yunior.eury@gmail.com>
2024-01-17 23:26:35 -0600

    -Manage multiple enemy nodes lifetimes. WIP.

--- Gunner/src/EnemyWaves_Circle.gd
+++ Gunner/src/EnemyWaves_Circle.gd
@@ -24,6 +24,7 @@ func configure(
 
 
 func spawn():
+	self.position.x += 100
 	var left_bound = 0
 	for i in range(number_of_enemies):
 		var enemy_instance: HealthBody2D = enemy.instantiate()
@@ -47,7 +48,37 @@ func _ready():
 #	anim_utils.zig_zag_pattern(
 #		self.curve, anim_utils.zig_zag_pattern(self.curve, Vector2(75, 83), 50, 5), -50, 5
 #	)
-	anim_utils.circle_pattern(self.curve, Vector2(75, 83), 45, 10, 4)
+#	anim_utils.circle_pattern(self.curve, Vector2(75, 83), 45, 10, 4)
+	var points = get_full_circle_shape_2dvectors_1phase(75, 75, Vector2(75, 0))
+	for p in points:
+		self.curve.add_point(p)
+
+
+func get_full_circle_shape_2dvectors_1phase(
+	x_radius: float, y_radius: float, origin: Vector2
+) -> Array:
+#	TODO:I think it's a matter of getting the all the points while the radius has not been covered?? Or going from o to 360n degrees.
+	var points = []
+	var i = 0
+	var current_sin_input_val = 0
+	var current_cos_input_val = 0
+	var x_speed: float = 0.05  #radians/spped/rate
+	var y_speed: float = 0.05  #radians/spped/rate
+
+#	Possible implementation...not quite right
+	while i < 1 * (2 * PI):
+#		Need to come up with all values for sin
+#		Need to come up with all values for cos
+		var temp_current_sinusoidal_output_val = y_radius * sin(current_sin_input_val)
+		var temp_current_cos_output_val = x_radius * cos(current_cos_input_val)
+		points.append(Vector2(temp_current_cos_output_val, temp_current_sinusoidal_output_val))
+		current_sin_input_val += y_speed
+		current_cos_input_val += x_speed
+		if y_speed < x_speed:
+			i += y_speed
+		else:
+			i += x_speed
+	return points
 
 
 func is_wave_alive():
