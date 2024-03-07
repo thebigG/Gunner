fb3721085f0069add45bb6cc15cb128b25ecab28
thebigG <yunior.eury@gmail.com>
2024-03-06 23:02:46 -0600

    -Add simple wasm server

---
+++ wasm_server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const port = process.env.PORT || 3000;
+app.use(function(req, res, next) {
+res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+next();
+});
+app.use(express.static("Gunner/exports/web/"));
+server.listen(port, function() {
+console.log("Listening on port:", port);
+}); 
