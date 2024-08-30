f500c8db13495f05995636dd6f5527074aa66f1e
thebigG <yunior.eury@gmail.com>
2024-08-29 22:58:59 -0500

    -Add ShootingRateItem powerup. WIP.

---
+++ Gunner/src/ShootingRateItem.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+var move_velocity = Vector2(0, 100)
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite2D/Area2D.connect("body_entered", heal_gunner)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	self.move_and_collide(move_velocity * delta)
+
+
+func heal_gunner(body):
+	if body.is_in_group("Gunner"):
+		body.heal()
+		queue_free()
