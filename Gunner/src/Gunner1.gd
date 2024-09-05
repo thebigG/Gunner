48c6a1d36f11fa3f2d5e1da109413a6db15e08ef
thebigG <yunior.eury@gmail.com>
2024-09-04 23:08:19 -0500

    -Add shooting rate to HUD. TODO:Present it in a user-friendly manner, "1.5X", "2X", etc. -Configure collision layers so that friendly bullets and Gunner do not collide.

--- Gunner/src/Gunner1.gd
+++ Gunner/src/Gunner1.gd
@@ -17,6 +17,7 @@ var hud: PanelContainer = null
 var hud_grid: GridContainer = null
 var hud_theme = preload("res://Assets/Themes/hud_theme.tres")
 var score_label: Label = Label.new()
+var current_shooting_rate_label: Label = Label.new()
 var level_progress: Label = Label.new()
 var score = 0
 var hurt_sprite_frames = SpriteFrames.new()
@@ -76,13 +77,17 @@ func _ready():
 
 	hud.theme = hud_theme
 	score_label.text = "Score:" + str(score)
+	current_shooting_rate_label.text = "Shooting Rate:"
 	level_progress.text = (
 		"Progress:"
 		+ str(get_tree().get_first_node_in_group("World3D").call("get_current_level_progress"))
 	)
 	hud_grid.add_child(score_label)
+
 	hud_grid.add_child(health_bar)
 	hud_grid.add_child(level_progress)
+
+	hud_grid.add_child(current_shooting_rate_label)
 
 	get_parent().get_parent().get_node("EasyStageScene").add_child(hud)
 
@@ -243,6 +248,8 @@ func _physics_process(delta):
 	hud.position.y = (
 		get_parent().get_parent().get_node("EasyStageScene/ParallaxDriver").position.y - hud_gap
 	)
+
+	current_shooting_rate_label.text = "Shooting Rate:" + str(desired_shooting_rate)
 
 
 func get_bullet_velocity():
